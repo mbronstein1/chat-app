c4ca7606c6df45e6ca89c7f41722b53f941a23b9
Matthew Bronstein <mbronstein1@gmail.com>
2023-02-07 10:34:48 -0600

    fixed backend socket.io connection w/ cors and added front-end socket.io connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
+
+let socket;
+const CONNECTION_PORT = 'http://localhost:3001';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [room, setRoom] = useState('');
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, []);
+
+  const connectToRoom = () => {
+    socket.emit('join_room', room);
+  };
 
   return (
     <div className='App'>
@@ -10,10 +24,12 @@ function App() {
       {!isLoggedIn ? (
         <div className='login'>
           <div className='inputs'>
-            <input type='text' placeholder='Name...' />
-            <input type='text' placeholder='Room...' />
+            <input type='text' placeholder='Name...' onChange={e => setUsername(e.target.value)} />
+            <input type='text' placeholder='Room...' onChange={e => setRoom(e.target.value)} />
           </div>
-          <button>Enter Chat</button>
+          <button type='button' onClick={connectToRoom}>
+            Enter Chat
+          </button>
         </div>
       ) : (
         <h1>You are logged in</h1>
--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-// const { Server } = require('socket.io');
+const { Server } = require('socket.io');
 const PORT = process.env.PORT || '3001';
 const app = express();
 
-app.use(cors);
+app.use(cors());
+app.use(express.json());
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+});
 
-// const server = new Server({
-//   cors: {
-//     origin: ['http://localhost:3000'],
-//   },
-// });
+io.on('connection', socket => {
+  console.log(socket.id);
+
+  socket.on('join_room', room => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
