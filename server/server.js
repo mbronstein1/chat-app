8330b57e4164637d0d789311229b1c15f509ead2
Matthew Bronstein <mbronstein1@gmail.com>
2023-02-21 18:52:42 -0600

    still experimenting

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')));
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
